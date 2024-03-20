c0fe490536af4b0d3c02383892ae6b79b9bcc3b2
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2024-03-20 20:09:04 +0100

    fix set-like for non-existant post

--- src/api/posts/{id}/set-like/_POST/index.ts
+++ src/api/posts/{id}/set-like/_POST/index.ts
@@ -10,6 +10,23 @@ export const setLike = errorCatcher(async (req: Request, res: Response) => {
     const { like: setLike } = req.body as { like: boolean };
     const { id: userId } = req.user as User;
 
+    const post = await prisma.post.findFirst({
+        where: {
+            id: postId,
+        },
+    });
+
+    if (!post)
+        return res.status(404).json({
+            errors: [
+                {
+                    message: 'Post not found',
+                    code: 'not_found',
+                    path: ['postId'],
+                },
+            ],
+        });
+
     const like = await prisma.like.findFirst({
         where: {
             postId,
