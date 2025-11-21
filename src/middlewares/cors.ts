6367f82a1003d1be896f9f47d80c37dacf659a9e
Adrian Rosicki <adrian.rosicki.9@gmail.com>
2025-11-21 22:22:40 +0100

    fix cors again

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -2,12 +2,16 @@ import { NextFunction, Request, Response } from 'express';
 import { ACCESS_CONTROL_ALLOW_ORIGIN } from '../config';
 
 export const cors = (req: Request, res: Response, next: NextFunction) => {
-    res.header('Access-Control-Allow-Origin', ACCESS_CONTROL_ALLOW_ORIGIN);
-    res.header(
+    const origins = ACCESS_CONTROL_ALLOW_ORIGIN.split(',');
+
+    origins.forEach(origin => {
+        res.header('Access-Control-Allow-Origin', origin);
+    });
+    res.header( 
         'Access-Control-Allow-Methods',
         'GET, POST, PATCH, DELETE, OPTIONS',
     );
-    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Credentials', 'true'); 
     res.header('Access-Control-Allow-Headers', ['content-type']);
     next();
 };
